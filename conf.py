fc12ccb26cccd7810b492b637b3cc6b0bbc0a85b
viktoria <leonieneon@gmail.com>
2017-02-05 17:30:26 +0100

    update config.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# :project_name documentation build configuration file, created by
+# Nightscout User Guide documentation build configuration file, created by
 # sphinx-quickstart on Mon Aug 17 11:31:18 2015.
 #
 # This file is execfile()d with the current directory set to its
@@ -63,9 +63,9 @@ source_encoding = 'utf-8-sig'
 master_doc = 'index'
 
 # General information about the project.
-project = u':project_name'
-copyright = u'2017, :author_name'
-author = u':author_name'
+project = u'Nightscout User Guide'
+copyright = u'2017, Nightscout Community'
+author = u'Nightscout Community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -217,7 +217,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = ':project_namedoc'
+htmlhelp_basename = 'Nightscout User Guidedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -239,8 +239,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, ':project_name.tex', u':project_name Documentation',
-   u':project_name', 'manual'),
+  (master_doc, 'Nightscout User Guide.tex', u'Nightscout User Guide Documentation',
+   u'Nightscout User Guide', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -269,7 +269,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, ':project_name', u':project_name Documentation',
+    (master_doc, 'Nightscout User Guide', u'Nightscout User Guide Documentation',
      [author], 1)
 ]
 
@@ -283,8 +283,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, ':project_name', u':project_name Documentation',
-   author, ':project_name', 'One line description of project.',
+  (master_doc, 'Nightscout User Guide', u'Nightscout User Guide Documentation',
+   author, 'Nightscout User Guide', 'One line description of project.',
    'Miscellaneous'),
 ]
 
